3fe5e3bea2ecae079d01943866aab340c41551e7
BHUVANESH CHOWDARY PUNATI <p.bhuvaneshchowdarybvpbc1255@gmail.com>
2026-09-08 21:48:16 +0530

    Sql Project of learning

--- Product Ranking.sql
+++ Product Ranking.sql
@@ -14,6 +14,7 @@ select * from (select *  ,  RANK() OVER(PARTITION BY product_category_name  orde
 
 select *  ,  DENSE_RANK() OVER(PARTITION BY product_category_name  order BY revenue DESC) as dense_rank_num from (select p.product_category_name, s.product_id,count(s.product_id) as sold,round(SUM(PRICE)+SUM(freight_value),2) as revenue from  olist_order_items_dataset s inner join olist_products_dataset p where s.product_id = p.product_id group by product_category_name, product_id) as t;
 
+# Using Rank products based on sales. Use ROW_NUMBER (), RANK (), and DENSE_RANK (). Find the top 5 products by revenue in each product category.
 
 select * from (SELECT product_category_name,product_id,sold,revenue,DENSE_RANK() OVER (PARTITION BY product_category_name ORDER BY revenue DESC) AS dense_rank_num FROM (
 SELECT p.product_category_name,s.product_id,COUNT(s.product_id) AS sold,ROUND(SUM(s.price)+SUM(freight_value), 2) AS revenue FROM olist_order_items_dataset s INNER JOIN olist_products_dataset p ON s.product_id = p.product_id GROUP BY p.product_category_name, s.product_id ) AS t ) as r
